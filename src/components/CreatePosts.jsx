0a0fd10af2110f8faceef6bee65d8bcb2acf90f4
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2023-12-07 15:24:50 +0100

    working on icons on home page

--- src/components/CreatePosts.jsx
+++ src/components/CreatePosts.jsx
@@ -1,145 +1,148 @@
 import { AddIcon } from "@chakra-ui/icons";
 import {
+	Button,
+	CloseButton,
+	Flex,
+	FormControl,
+	Image,
+	Input,
 	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalFooter,
 	ModalBody,
 	ModalCloseButton,
-	Button,
-	useDisclosure,
-	FormControl,
-	Textarea,
-	CloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
 	Text,
-	Flex,
-	Input,
-	Image,
-
+	Textarea,
+	useColorModeValue,
+	useDisclosure,
 } from "@chakra-ui/react";
-import { useState, useRef } from "react";
-import usePreviewImg from "../hooks/usePreviewImg"
-import {BsFillImageFill} from "react-icons/bs"
-import userAtom from "../atoms/userAtom"
-import { useRecoilValue, useRecoilState } from "recoil";
+import { useRef, useState } from "react";
+import usePreviewImg from "../hooks/usePreviewImg";
+import { BsFillImageFill } from "react-icons/bs";
+import { useRecoilState, useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import useShowToast from "../hooks/useShowToast";
 import postsAtom from "../atoms/postsAtom";
 import { useParams } from "react-router-dom";
-import useShowToast from "../hooks/useShowToast";
 
+const MAX_CHAR = 500;
 
-
-const MAX_CHAR = 500
-
-const CreatePosts = () => {
+const CreatePost = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
-	const [postText, setPostText] = useState("")
+	const [postText, setPostText] = useState("");
+	const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
 	const imageRef = useRef(null);
-	const [remainingChar, setRemainingChar] = useState(MAX_CHAR)
-	const {handleImageChange, imgUrl, setImgUrl} = usePreviewImg()
-	const [loading, setLoading] = useState(false)
-	const user = useRecoilValue(userAtom)
-	const showToast = useShowToast()
-	const [posts, setPosts] = useRecoilState(postsAtom)
-	const {username} = useParams()
+	const [remainingChar, setRemainingChar] = useState(MAX_CHAR);
+	const user = useRecoilValue(userAtom);
+	const showToast = useShowToast();
+	const [loading, setLoading] = useState(false);
+	const [posts, setPosts] = useRecoilState(postsAtom);
+	const { username } = useParams();
 
-	const handleTextChange = (e) => {
-		const inputText = e.target.value
-		if(inputText.lenght > MAX_CHAR) {
-			const truncatedText = inputText.slice(0, MAX_CHAR)
-			setPostText(truncatedText)
-			setRemainingChar(0)
+	const handleTextChange = (e) => { 
+		const inputText = e.target.value;
+
+		if (inputText.length > MAX_CHAR) {
+			const truncatedText = inputText.slice(0, MAX_CHAR);
+			setPostText(truncatedText);
+			setRemainingChar(0);
 		} else {
-			setPostText(inputText)
-			setRemainingChar(MAX_CHAR - inputText.length)
+			setPostText(inputText);
+			setRemainingChar(MAX_CHAR - inputText.length);
 		}
-	}
+	};
 
 	const handleCreatePost = async () => {
 		setLoading(true);
-			try{
-				const res = await fetch("/api/posts/create", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({postedBy: user._id, text: postText, img: imgUrl}),
-				})
-					const data = await res.json()
-					if(data.error){
-						showToast("Error", data.error, "error")
-						return
-					}
+		try {
+			const res = await fetch("/api/posts/create", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ postedBy: user._id, text: postText, img: imgUrl }),
+			});
 
-					showToast("Success", "Post Created Successfully", "Success")
-					if(username === user.username) {
-						setPosts([data, ...posts]);
-					}
-
-					onClose()
-
-			}catch(error){
-				showToast("Error", error, "Error")
-			} finally {
-				setLoading(false)
+			const data = await res.json();
+			if (data.error) {
+				showToast("Error", data.error, "error");
+				return;
 			}
-	}
+			showToast("Success", "Post created successfully", "success");
+			if (username === user.username) {
+				setPosts([data, ...posts]);
+			}
+			onClose();
+
+			
+			setPostText("");
+			setImgUrl("");
+		} catch (error) {
+			showToast("Error", error, "error");
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<>
 			<Button
-				pos={"fixed"}
+				position={"fixed"}
 				bottom={10}
-				right={10}
-				leftIcon={<AddIcon />}
-				bg={"gray"}
-                onClick={onOpen}
-			>	
-				Post
+				right={5}
+				bg={useColorModeValue("gray.300", "gray.dark")}
+				onClick={onOpen}
+				size={{ base: "sm", sm: "md" }}
+			>
+				<AddIcon />
 			</Button>
+
 			<Modal isOpen={isOpen} onClose={onClose}>
 				<ModalOverlay />
+
 				<ModalContent>
-					<ModalHeader>Create post</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody  pd={6}>
-                            <FormControl>
-								<Textarea
-									placeholder="Post Content goes here"
-									onChange={handleTextChange}
-									value={postText}
-								/>
-								<Text fontSize="xs" fontWeight="bold" textAlign={"right"} m={"1"} color={"gray.800 "}>
-									{remainingChar}/{MAX_CHAR}
-								</Text>
-								<Input type="file" hidden ref={imageRef} onChange={handleImageChange}/>
+					<ModalHeader>Create Post</ModalHeader> 
+					<ModalCloseButton /> 
+					<ModalBody pb={6}>
+						<FormControl>
+							<Textarea
+								placeholder='Post content goes here..'
+								onChange={handleTextChange}
+								value={postText}
+							/>
+							<Text fontSize='xs' fontWeight='bold' textAlign={"right"} m={"1"} color={"gray.800"}>
+								{remainingChar}/{MAX_CHAR}
+							</Text>
 
-								<BsFillImageFill 
-									style={{marginLeft: "5px", cursor: "pointer"}}
-									size={16}
-									onClick={() => imageRef.current.click()}
-								/>
-							</FormControl>
+							<Input type='file' hidden ref={imageRef} onChange={handleImageChange} />
 
-							{imgUrl && (
-								<Flex
-									mt={"full"} position={"relative"} 
-								>
-								<Image src={imgUrl} alt="select img" /> 
-									<CloseButton 
-										onClick={() => {
-											setImgUrl("");
-										}}
-										bg={"gray.800"}
-										position={"absolute"}
-										top={2}
-										right={2}
-									/>	
-								</Flex>
-							)}
+							<BsFillImageFill
+								style={{ marginLeft: "5px", cursor: "pointer" }}
+								size={16}
+								onClick={() => imageRef.current.click()}
+							/>
+						</FormControl>
+
+						{imgUrl && (
+							<Flex mt={5} w={"full"} position={"relative"}>
+								<Image src={imgUrl} alt='Selected img' />
+								<CloseButton
+									onClick={() => {
+										setImgUrl("");
+									}}
+									bg={"gray.800"}
+									position={"absolute"}
+									top={2}
+									right={2}
+								/>
+							</Flex>
+						)}
 					</ModalBody>
 
 					<ModalFooter>
-						<Button colorScheme="blue" mr={3} onClick={handleCreatePost} isLoading={loading}>
+						<Button colorScheme='blue' mr={3} onClick={handleCreatePost} isLoading={loading}>
 							Post
 						</Button>
 					</ModalFooter>
@@ -149,4 +152,4 @@ const CreatePosts = () => {
 	);
 };
 
-export default CreatePosts;
+export default CreatePost;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,42 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue} from "recoil";
+import userAtom from "../atoms/userAtom"; 
+import {AiFillHome} from "react-icons/ai"
+import {RxAvatar} from "react-icons/rx"
+import {Link as RouterLink} from "react-router-dom"
+// import RxAvatar from "./RxAvatar"
+
 
 const Header = () => {
    const { colorMode, toggleColorMode } = useColorMode();
+   const user = useRecoilValue(userAtom);
 
    return (
-      <Flex justifyContent={"center"} mt={6} mb="12">
+      <Flex justifyContent={"space-between"} mt={6} mb="12">
+            {user && (
+               <Link as={RouterLink} to="/">
+                  <AiFillHome size={24} />
+               </Link>
+            )}
+
+            {/* {!user && (
+               <Link as={RouterLink} to={"/auth"} onClick={() =>  setAuthScreen("login")}>
+                  Login
+               </Link>
+            )} */}
          <Image
             cursor="pointer"
             alt="logo"
             w={6}
             src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
             onClick={toggleColorMode}
-         />
+         /> 
+
+         {user && (
+            <Link as={RouterLink} to= {`/${user.username}`}>
+            <RxAvatar size={24} />   
+            </Link>
+         )}
       </Flex>
    );
 };
