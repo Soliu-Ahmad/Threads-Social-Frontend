8154f7b3c819a9a5a4613268f0279e80cc2a48a1
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2023-12-07 10:21:19 +0100

    working on create post

--- src/components/CreatePosts.jsx
+++ src/components/CreatePosts.jsx
@@ -9,11 +9,25 @@ import {
 	ModalCloseButton,
 	Button,
 	useDisclosure,
+	FormControl,
+	Textarea,
+	Text,
+	Input
 } from "@chakra-ui/react";
+import { useState, useRef } from "react";
+import usePreviewImg from "../hooks/usePreviewImg"
+import {BsFillImageFill} from "react-icons/bs"
 
 
 const CreatePosts = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [postText, setPostText] = useState("")
+	const imageRef = useRef(null);
+	const {handleImageChange, imgUrl} = usePreviewImg()
+
+	const handleTextChange = () => {
+
+	}
 	return (
 		<>
 			<Button
@@ -23,23 +37,40 @@ const CreatePosts = () => {
 				leftIcon={<AddIcon />}
 				bg={"gray"}
                 onClick={onOpen}
-			>
+			>	
 				Post
 			</Button>
 			<Modal isOpen={isOpen} onClose={onClose}>
 				<ModalOverlay />
 				<ModalContent>
-					<ModalHeader>Modal Title</ModalHeader>
+					<ModalHeader>Create post</ModalHeader>
 					<ModalCloseButton />
-					<ModalBody>
-                            Here we go
+					<ModalBody  pd={6}>
+                            <FormControl>
+								<Textarea
+									placeholder="Post Content goes here"
+									onChange={handleTextChange}
+									value={postText}
+								/>
+								<Text fontSize="xs" fontWeight="bold" textAlign={"right"} m={"1"} color={"gray.800 "}>
+									500/500
+								</Text>
+								<Input type="file" hidden ref={imageRef} onChange={handleImageChange}/>
+
+								<BsFillImageFill 
+									style={{marginLeft: "5px", cursor: "pointer"}}
+									size={16}
+									onClick={() => imageRef.current.click()}
+								/>
+							</FormControl>
+
+							
 					</ModalBody>
 
 					<ModalFooter>
 						<Button colorScheme="blue" mr={3} onClick={onClose}>
 							Close
 						</Button>
-						<Button variant="ghost">Secondary Action</Button>
 					</ModalFooter>
 				</ModalContent>
 			</Modal>
